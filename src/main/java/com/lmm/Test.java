e84ed3703ec34d0e9d6cb9c5165c60c58c55ef9a
刘蒙蒙 <liumengmeng@liumengmengdeMacBook-Pro.local>
2018-12-22 22:43:12 +0800

    Initial commit

---
+++ src/main/java/com/lmm/Test.java
@@ -0,0 +1,15 @@
+package com.lmm;
+
+/**
+ * @author
+ * @create 2018/12/22- 下午8:47
+ */
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println(1111);
+
+        System.out.println("HelloWorld");
+
+    }
+}
